375ec768e8cf81c7e3f5d52e7bac6697f12785ad
Eraysame <cabdiqaniforex@gmail.com>
2025-03-21 01:19:37 +0300

    added data entery

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 class CSV:
     CSV_FILE = "finance_date.csv"
-    columns = ["Date", "Amount", "Categroy", "description"]
+    columns = ["date", "Amount", "Categroy", "description"]
 
     @classmethod
     def initialize_csv(cls):
@@ -17,16 +17,18 @@ class CSV:
             df.to_csv(cls.CSV_FILE, index=False)
 
     @classmethod
-    def add_entry(cls, Date, amount, Categroy, description):
+    def add_entry(cls, date, Amount, Categroy, description):
         new_entry = {
-            "Date": Date,
-            "amount": amount,
+            "date": date,
+            "Amount": Amount,
             "Categroy": Categroy,
             "description": description
         }
         with open(cls.CSV_FILE, "a", newline="") as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=cls.columns)
             writer.writerow(new_entry)
+        print("Entery added successfully")
 
 
 CSV.initialize_csv()
+CSV.add_entry("20-07-2024", 123.05, "income", "salary")
