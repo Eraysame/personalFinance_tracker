68a9c8efaf813876a65dcdb9c7261bb5f1410d48
Eraysame <cabdiqaniforex@gmail.com>
2025-03-21 22:56:01 +0300

    added

--- data_entry.py
+++ data_entry.py
@@ -1,25 +1,39 @@
 from datetime import datetime
 
+date_format = "%d-%m-%Y"
+categries = {"I": "Income", "E": "Expenses"}
+
 
 def get_date(prompt, allow_defualt=False):
     date_str = input(prompt)
     if allow_defualt and not date_str:
-        return datetime.today().strftime("%d-%m-%Y")
+        return datetime.today().strftime(date_format)
 
     try:
-        valid_date = datetime.strptime(date_str, "%d-%m-%Y")
-        return valid_date.strptime(date_str, "%d-%m-%Y")
+        valid_date = datetime.strptime(date_str, date_format)
+        return valid_date.strftime(date_format)
     except ValueError:
         print("Invalid date format. please enter date in dd-mm-yy format!")
         return get_date(prompt, allow_defualt)
 
 
 def get_amount():
-    pass
+    try:
+        amount = float(input("please enter amount: "))
+        if amount <= 0:
+            raise ValueError("Amount must be non-negitive non-zero value")
+        return amount
+
+    except ValueError as e:
+        print(e)
+        return get_amount()
 
 
 def get_category():
-    pass
+    category = input(
+        "enter the catogry ('I' for income or 'E' for expenses )").upper()
+    if category in categries:
+        return categries()
 
 
 def get_description():
